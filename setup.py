eed3f32aa578b78999c8df3618b3fa452cfa1c8a
benVar <beniza@gmail.com>
2017-12-14 15:25:32 +0530

    Expand setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,16 @@
 from setuptools import setup
+'''
+Tool to automatically tag Chain References and Cross References
+'''
 
 setup(
-    name="crossreftag",
-    description="###",
-    version="###",
+    name="CrossRefTag",
+    description="To automatically tag Chain Bible References\
+    and Cross Refernces with info from Biblical Terms",
+    long_description=open("README.md").read(),
+    version="0.1",
+    license="MIT",
+    packages=["CrossRefTag",],
     install_requires=[
         "joblib"
         ],
