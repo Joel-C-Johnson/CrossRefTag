de75a3878c05c86af706dd444fccb9382802af43
Joel C. Johnson <joelcjohnson123@gmail.com>
2017-12-15 17:02:33 +0530

    Updated thefile

--- CrossRefTag/CrossRefTag.py
+++ CrossRefTag/CrossRefTag.py
@@ -4,6 +4,12 @@ import re
 import string
 import nltk
 import pdb
+import glob
+from joblib import Parallel, delayed
+import time
+import sys
+import os
+
 
 tree1 = ET.parse('ProjectBiblicalTerms.xml')
 root1 = tree1.getroot()
@@ -42,7 +48,7 @@ for t in root1.findall('Term'):
             main_dict[verse_code] = temp_dict
     term_reference_dict[word] = term_reference_list
 
-books = {"1": "GEN", "2": "EXO", "3": "LEV", "4": "NUM", "5": "DEU", "6": "JOS", "7": "JDG", "8": "RTH", "9": "1SA", "10": "2SA", "11": "1KI", "12": "2KI", "13": "1CH", "14": "2CH", "15": "EZR", "16": "NEH", "17": "EST", "18": "JOB", "19": "PSA", "20": "PRO", "21": "ECC", "22": "SNG", "23": "ISA", "24": "JER", "25": "LAM", "26": "EZE", "27": "DAN", "28": "HOS", "29": "JOE", "30": "AMO", "31": "OBA", "32": "JON", "33": "MIC", "34": "NAH", "35": "HAB", "36": "ZEP", "37": "HAG", "38": "ZEC", "39": "MAL", "40": "MAT", "41": "MAR", "42": "LUK", "43": "JHN", "44": "ACT", "45": "ROM", "46": "1CO", "47": "2CO", "48": "GAL", "49": "EPH", "50": "PHL", "51": "COL", "52": "1TH", "53": "2TH", "54": "1TI", "55": "2TI", "56": "TIT", "57": "PHM", "58": "HEB", "59": "JAS", "60": "1PE", "61": "2PE", "62": "1JO", "63": "2JO", "64": "3JO", "65": "JUD", "66": "REV"}
+books = {"1": "GEN", "2": "EXO", "3": "LEV", "4": "NUM", "5": "DEU", "6": "JOS", "7": "JDG", "8": "RUT", "9": "1SA", "10": "2SA", "11": "1KI", "12": "2KI", "13": "1CH", "14": "2CH", "15": "EZR", "16": "NEH", "17": "EST", "18": "JOB", "19": "PSA", "20": "PRO", "21": "ECC", "22": "SNG", "23": "ISA", "24": "JER", "25": "LAM", "26": "EZK", "27": "DAN", "28": "HOS", "29": "JOL", "30": "AMO", "31": "OBA", "32": "JON", "33": "MIC", "34": "NAM", "35": "HAB", "36": "ZEP", "37": "HAG", "38": "ZEC", "39": "MAL", "40": "MAT", "41": "MRK", "42": "LUK", "43": "JHN", "44": "ACT", "45": "ROM", "46": "1CO", "47": "2CO", "48": "GAL", "49": "EPH", "50": "PHP", "51": "COL", "52": "1TH", "53": "2TH", "54": "1TI", "55": "2TI", "56": "TIT", "57": "PHM", "58": "HEB", "59": "JAS", "60": "1PE", "61": "2PE", "62": "1JN", "63": "2JN", "64": "3JN", "65": "JUD", "66": "REV"}
 books_inverse = {v:k for k,v in books.items()} 
 
 def check_pattern(tr_word, tr_wordlist):
@@ -71,7 +77,10 @@ def tag_format(code, verse_list):
     next_index = None
     if code in verse_list:
         index_value = verse_list.index(code)
-        if index_value == 0:
+        if len(verse_list) == 1:
+            previous_index = None
+            next_index = None
+        elif index_value == 0:
             next_index = index_value + 1
         elif index_value > 0 and (index_value + 1) == len(verse_list):
             previous_index = index_value - 1
@@ -90,7 +99,7 @@ def tag_format(code, verse_list):
         next_code = books[str(int(verse_list[next_index][0:3]))] + ' ' + str(int(verse_list[next_index][3:6])) + ':' + str(int(verse_list[next_index][6:9]))
     else:
         next_code = ''
-    formatted = "\\f + \\fr " + str(current_code) +" \\ft Previous: " + str(previous_code) + "; Next: " + str(next_code) + "\\ft*"
+    formatted = "\\fSPT+SPT\\fr " + str(current_code) +" \\ft Previous: " + str(previous_code) + "; Next: " + str(next_code) + "\\ft*"
     return formatted
 
 def verse_handler(code, verse):
@@ -99,75 +108,47 @@ def verse_handler(code, verse):
     final_verse = '\\v ' + str(verse_num) + ' ' + verse
     return final_verse
 
-# def add_tags(verse_code, verse):
-#     'returns a text with the tags added to it.'
-    
-#     punct_handler = re.sub(r'\s([' + string.punctuation +'])', r'SPT\1', verse)
-#     punct_handler = re.sub(r'([' + string.punctuation +'])\s', r'\1SPT', punct_handler)
-#     punct_handler = re.sub(r'([' + string.punctuation +'])', r' \1 ', punct_handler)  #SPT is a marker
-#     word_list = nltk.word_tokenize(punct_handler)
-#     main_word_list = []
-#     for wi in word_list:
-#         if verse_code not in main_dict:
-#             main_word_list.append(wi)
-#         else:
-#             for k, v in main_dict[verse_code].items():
-#                 tranlsation_words = check_pattern(wi, v)    #Recieves all the available translations/interpretations of the word, if any.
-#                 if tranlsation_words:
-#                     verse_text = wi + tag_format(verse_code, term_reference_dict[k]) #wi is the actual word and tag_format() returns a text with information of the previous and next occurences.
-#                     main_word_list.append(verse_text)
-#                     break
-#                 else:
-#                     main_word_list.append(wi)
-#                     break
-#     join_main_word_list = " ".join(main_word_list)
-#     main_word_text = re.sub(r"``", '"', join_main_word_list)
-#     main_word_text = re.sub(r'\s([' + string.punctuation + '])\s', r'\1', main_word_text)
-#     main_word_text = re.sub(r'SPT', ' ', main_word_text)
-#     # print (main_word_text)
-#     return main_word_text
-
-#******************************************************************
-
 def add_tags(verse_code_tuple, verse):
-        'returns a text with the tags added to it.'
-        # print (verse_code_tuple)
-        punct_handler = re.sub(r'\s([' + string.punctuation +'])', r'SPT\1', verse)
-        punct_handler = re.sub(r'([' + string.punctuation +'])\s', r'\1SPT', punct_handler)
-        punct_handler = re.sub(r'([' + string.punctuation +'])', r' \1 ', punct_handler)  #SPT is a marker
-        word_list = nltk.word_tokenize(punct_handler)
-        main_word_list = []
-        verse_code_length = int(verse_code_tuple[1]) + 1
-        verse_code = verse_code_tuple[0]
-        # pdb.set_trace()
-        temp_word_dict = {}
-        for i in range(0, int(verse_code_length)):
-            temp_verse_code = '0' + str(int(verse_code) + i)
-            if temp_verse_code in main_dict:
-                for k, v in main_dict[temp_verse_code].items():
-                    temp_word_dict[k] = v
-        # print (temp_word_dict)
-        for wi in word_list:
-            if verse_code not in main_dict:
-                main_word_list.append(wi)
+    '''Recieves a tuple with verse code and number(string) and the verse as 
+    argumenats Each word in the verse is looked up against a list of the bible 
+    terms of that specific verse for a match and returns a text with the chain 
+    reference formatted tags added to it.'''
+    punct_handler = re.sub(r'\s([' + string.punctuation +'])', r'SPT\1', verse)
+    punct_handler = re.sub(r'([' + string.punctuation +'])\s', r'\1SPT', punct_handler)
+    punct_handler = re.sub(r'([' + string.punctuation +'])', r' \1 ', punct_handler)  #SPT is a marker
+    word_list = nltk.word_tokenize(punct_handler)
+    main_word_list = []
+    verse_code_length = int(verse_code_tuple[1]) + 1
+    verse_code = verse_code_tuple[0]
+    book_code = verse_code[0:3]
+    chapter_code = verse_code[3:6]
+    refer_code = verse_code[6:9]
+    verse_code_list = []
+    for i in range(0, verse_code_length):
+        current_value = book_code + chapter_code + digit_lenght_check(str(int(refer_code) + i)) + '00'
+        verse_code_list.append(current_value)
+    for word in word_list:
+        word_list_len = len(main_word_list)
+        if word in string.punctuation:
+            main_word_list.append(word)
+            continue
+        for v_code in verse_code_list:
+            if v_code not in main_dict:
+                continue
             else:
-                for k, v in temp_word_dict.items():
-                    tranlsation_words = check_pattern(wi, v)    #Recieves all the available translations/interpretations of the word, if any.
-                    if tranlsation_words:
-                        verse_text = wi + tag_format(verse_code, term_reference_dict[k]) #wi is the actual word and tag_format() returns a text with information of the previous and next occurences.
+                for k, v in main_dict[v_code].items():
+                    translation_check = check_pattern(word, v)
+                    if translation_check:
+                        verse_text = word + tag_format(v_code, term_reference_dict[k])
                         main_word_list.append(verse_text)
                         break
-                    else:
-                        main_word_list.append(wi)
-                        break
-        join_main_word_list = " ".join(main_word_list)
-        main_word_text = re.sub(r"``", '"', join_main_word_list)
-        main_word_text = re.sub(r'\s([' + string.punctuation + '])\s', r'\1', main_word_text)
-        main_word_text = re.sub(r'SPT', ' ', main_word_text)
-        # print (main_word_text)
-        return main_word_text
-
-    #*******************************************************************8
+        if len(main_word_list) == word_list_len:
+            main_word_list.append(word)
+    join_main_word_list = " ".join(main_word_list)
+    main_word_text = re.sub(r"``", '"', join_main_word_list)
+    main_word_text = re.sub(r'\s([' + string.punctuation + '])\s', r'\1', main_word_text)
+    main_word_text = re.sub(r'SPT', ' ', main_word_text)
+    return main_word_text
 
 def usfm_handler(filename):
     'To process the usfm files'
@@ -178,9 +159,6 @@ def usfm_handler(filename):
     book_name = re.search('(?<=\id )\w{3}', file_text).group(0)
     book_num = books_inverse[book_name]
     book_code = digit_lenght_check(book_num)
-    file_text = re.sub('\*', 'asterix', file_text)  #Needs handling ************
-    file_text = re.sub('\(', 'opening_bracket', file_text)  #Needs handling ************
-    file_text = re.sub('\)', 'closing_bracket', file_text)  #Needs handling ************
     split_usfm = file_text.split('\\c')
     i = 0
     for chapter_text in split_usfm:
@@ -195,18 +173,13 @@ def usfm_handler(filename):
             verse_pattern = re.compile(r'(\\v )(\d+)(.*)')
             verse_pattern2 = re.compile(r'(\\v )(\d+)[abcd]?-(\d+)[abcd]?')
             split_text = chapter_text.split('\\v')
-            # print (split_text[1])
             main_text_list.append(split_text.pop(0))
-            # print (split_text.pop())
-            # print (split_text[1])
             for line in split_text:
                 
                 other_filter_pattern = re.compile(r'\n(\\)(.*)')
                 line = re.sub(other_filter_pattern, r'SPTnewlineslSPT\2', line)
                 line = "\\v" + line
-                # print (line)
                 result = re.match(verse_pattern, line)
-                # pdb.set_trace()
                 if result:
                     verse = result.group(3)
                     hypen_verse = re.match(verse_pattern2, line)
@@ -215,8 +188,7 @@ def usfm_handler(filename):
                         len_reference_code = int(hypen_verse.group(3)) - int(hypen_verse.group(2))
                         first_verse = str(book_code) + str(chapter_code) + digit_lenght_check(hypen_verse.group(2)) + '00'
                         verse_code = (first_verse, str(len_reference_code))
-                        print (verse_code)
-                    else: #re.match(reference_pattern1, line):
+                    else:
                         reference_code = digit_lenght_check(result.group(2))
                         verse_sub = re.sub(verse_pattern, r'' + str(book_code) + str(chapter_code) + str(reference_code) + '00' + str(verse), line)
                         verse_code = (verse_sub[0:11], '0')
@@ -226,13 +198,16 @@ def usfm_handler(filename):
                 else:
                     main_text_list.append(line)
     final_text = "\n".join(main_text_list)
-    final_text = re.sub('opening_bracket', r'(', final_text)
-    final_text = re.sub('closing_bracket', r')', final_text)
     final_text = re.sub(' newlinesl ', r'\n\\', final_text)
-
-    final_text = re.sub('asterix', '*', final_text) 
-    open_file = open('output' + str(filename), 'w')
+    final_text = re.sub(r'\\v (\d+) - (\d+)', r'\\v \1-\2', final_text)
+    open_file = open('output/' + str(filename), 'w')
     open_file.write(final_text)
     return 'finished'
 
-usfm_handler('66_JUD-OA-HIN.usfm')
+def start():
+    now = time.time()
+    j = Parallel(n_jobs=4, verbose=1, backend="multiprocessing") (delayed (usfm_handler)(filename) for filename in glob.glob('usfmfiles/*.SFM') )
+    print ("Finished in", time.time()-now , "sec")
+
+if __name__ == '__main__':
+    start()
